7dc6021e4b91c7a66f5f5d6944f38c85efcea1de
JamesLee <48775473+titanian229@users.noreply.github.com>
2020-07-24 19:17:41 -0400

    Initialized component files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { withRouter } from 'react-router';
+
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Portfolio from './pages/Portfolio'
+
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+
+const NavbarWithRouter = withRouter(Navbar)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+    <Router>
+        <Route exact path="/" component={Header} />
+        <NavbarWithRouter />
+        <Route exact path="/" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+    </Router>
+    );
 }
 
 export default App;
